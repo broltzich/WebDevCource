c2fbf5b117dedb032cfb4d55331f8d92bb60acd1
Leo <add.object@bk.ru>
2017-10-25 23:40:44 +0300

    ex4 complete

--- lab4-master/ex_4.py
+++ lab4-master/ex_4.py
@@ -1,7 +1,10 @@
+# -*- :coding utf-8 -*-
+
 from librip.decorators import print_result
 
-# Необходимо верно реализовать print_result
-# и задание будет выполнено
+
+
+
 
 @print_result
 def test_1():
--- lab_3/tests.py
+++ lab_3/tests.py
@@ -1,21 +1,2 @@
-# -*- coding: utf-8 -*-
+# -*- :coding utf-8 -*-
 
-class Imp:
-    def __init__(self, lst):
-        self.lst = lst
-        self.index = 0
-
-    def next(self):
-        if self.index == len(self.lst) - 1:
-            raise StopIteration
-        self.index += 1
-        return self.lst[self.index]
-
-    def __iter__(self):
-        # Чтобы можно было вызывать iter() над нашим объектом
-        return self
-
-
-arl = Imp([1, 2, 3, 4])
-for i in arl:
-    print i
